955fc6d7986aa9bb8d8a5323bd1d86c81860ac57
Nedyalko Dyakov <nedyalko.dyakov@gmail.com>
2020-03-01 12:11:10 +0200

    nah, remove week number

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -39,9 +39,8 @@ function fish_right_prompt
 
   set -l path (prompt_pwd)
   set -l directory_info "$path"
-  set -l week_number (date "+%W/52")
 
-  echo -n -s "$git_info $directory_info | $week_number"
+  echo -n -s "$git_info $directory_info"
 end
 
 
