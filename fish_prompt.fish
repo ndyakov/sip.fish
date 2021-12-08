79b1f1f50f6a270b6e48ff2ad38cca91b158101e
Nedyalko Dyakov <nedyalko.dyakov@gmail.com>
2021-12-08 14:18:51 +0200

    work with new fish, please.

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -3,11 +3,11 @@ function fish_mode_prompt; end
 function fish_greeting; end
 
 function _git_branch_name
-  echo (command git branch --no-color)
+  echo (command git symbolic-ref HEAD 2>/dev/null | sed -e 's|^refs/heads/||')
 end
 
 function _is_git_dirty
-  echo (command git status -s --ignore-submodules=dirty ^/dev/null)
+  echo (command git status -s --ignore-submodules=dirty 2>/dev/null)
 end
 
 function fish_right_prompt
